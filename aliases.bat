70a7abdcdeb20b12952d37268795aeeb9aeebf68
Drew Knab <drewknab@gmail.com>
2017-12-05 15:42:53 -0500

    Create aliases.bat
    Aliases when working on a windows machine.

---
+++ aliases.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: Commands
+
+DOSKEY ls=dir /B
+DOSKEY alias=gvim %USERPROFILE%\code\fun\aliases.bat
+
+:: Movement
+DOSKEY ~=cd "%USERPROFILE%"
+DOSKEY code=cd "%USERPROFILE%\code\$*"
+
+:: Git Commands
+DOSKEY gca=git commit -a -m"$*"
+DOSKEY gp=git push
+DOSKEY gs=git status
