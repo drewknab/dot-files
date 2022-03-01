7a1cd5d223eea541f6e3a7564db21d59dadb977a
Drew Knab <drewknab@gmail.com>
2022-03-01 10:11:05 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -15,3 +15,25 @@ function prompt {
 
     return "$ "
 }
+
+function showGitStatus { & git status $args}
+function showGitLog { & git log $args}
+function showGitLogPretty { & git log --graph --oneline --decorate}
+function doGitCheckoutMain { & git checkout main $args}
+function doGitCheckoutDev { & git checkout dev $args}
+function doGitAddAll{ & git add . $args}
+function doGitCommitMessage {& git commit -m $args}
+function doGitCommitAllMessage {& git commit -a -m $args}
+function doGitPush {& git push $args}
+function doGitClean {& git reset --hard && git clean -df}
+
+Set-Alias -Name gst -Value showGitStatus
+New-Alias -Name gln -Value showGitLog
+New-Alias -Name glhf -Value showGitLogPretty
+New-Alias -Name gcom -Value doGitCheckoutMain
+New-Alias -Name gcod -Value doGitCheckoutDev
+New-Alias -Name gaa -Value doGitAddAll
+New-Alias -Name gcwm -Value doGitCommitMessage
+New-Alias -Name gca -Value doGitCommitAllMessage
+New-Alias -Name gpu -Value doGitPush
+New-Alias -Name nah -Value doGitClean
